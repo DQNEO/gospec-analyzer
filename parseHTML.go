3f92fe4afe33117ba8014734161522feab4bdfac
DQNEO <dqneoo@gmail.com>
2021-01-29 18:22:58 +0900

    Manipulatte DOM

--- parseHTML.go
+++ parseHTML.go
@@ -1,12 +1,27 @@
 package main
 
-import "io/ioutil"
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"github.com/PuerkitoBio/goquery"
+)
 
 func main() {
-	html, err := ioutil.ReadFile("./spec.html")
+	//html, err := ioutil.ReadFile("./spec.html")
+	//if err != nil {
+	//	panic(err)
+	//}
+	//fmt.Print(string(html))
+	specFile := os.Getenv("GOPATH") + "/src/github.com/DQNEO/go-samples/nlp/gospec/spec.html"
+	f, err := os.Open(specFile)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Print(string(html))
+	doc, err := goquery.NewDocumentFromReader(f)
+	if err != nil {
+		panic(err)
+	}
+	doc.Find("pre").Remove()
+	noCodeText := doc.Text()
+	fmt.Print(noCodeText)
 }
