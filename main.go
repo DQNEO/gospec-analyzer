edfbce51d98082483bd51d5f825a11658078e153
DQNEO <dqneoo@gmail.com>
2021-04-19 22:07:56 +0900

    Improve cmd options

--- main.go
+++ main.go
@@ -1,5 +1,3 @@
-// Usage:
-//   go run main.go
 package main
 
 import (
@@ -12,7 +10,33 @@ import (
 
 var specFile = os.Getenv("GOPATH") + "/src/github.com/DQNEO/go-samples/nlp/gospec/spec.html"
 
+func showUsage() {
+	help := `
+Usage:
+	dump : show tokens
+	count: show statistics
+`
+	fmt.Print(help)
+}
 func main() {
+	if len(os.Args) == 1 {
+		showUsage()
+		return
+	}
+	arg := os.Args[1]
+
+	var modeDump bool
+	var modeCount bool
+	switch arg {
+	case "dump":
+		modeDump = true
+	case "count":
+		modeCount = true
+	default:
+		showUsage()
+		return
+	}
+
 	text := extractText(specFile)
 	//fmt.Print(text)
 
@@ -26,15 +50,22 @@ func main() {
 
 	// Iterate over the gdoc's tokens:
 	for _, tok := range doc.Tokens() {
-		//fmt.Println(tok.Text, tok.Tag, tok.Label)
-		// Go NNP B-GPE
-		// is VBZ O
-		// an DT O
-		// ...
 		word := strings.ToLower(tok.Text)
 		wordCount[word]++
+		if modeDump {
+			// Go NNP B-GPE
+			// is VBZ O
+			// an DT O
+			// ...
+			fmt.Println(tok.Text, tok.Tag, tok.Label)
+		}
 	}
+	if modeCount {
+		showCount(wordCount)
+	}
+}
 
+func showCount(wordCount map[string]int) {
 	type sameFreqGroup []string
 	var frequency []sameFreqGroup = make([]sameFreqGroup, len(wordCount))
 	for w, n := range wordCount {
@@ -58,4 +89,5 @@ func main() {
 		}
 	}
 	fmt.Printf("%4d\tTotal\n", total)
+
 }
