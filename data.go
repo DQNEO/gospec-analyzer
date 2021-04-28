bd26a1684d8b2a291441ffbe9ba0e69b96267358
DQNEO <dqneoo@gmail.com>
2021-04-28 23:33:21 +0900

    separate data

---
+++ data.go
@@ -0,0 +1,31 @@
+package main
+
+var meaninglessTokens = map[string]bool{
+	"CD":true,   // cardinal number
+	"DT":true,   // determiner
+	"IN":true,   // conjunction, subordinating or preposition
+	"CC":true,   // conjunction, coordinating
+	"PRP":true,  // pronoun, personal
+	"PRP$":true, // pronoun, possessive
+	"TO":true,   // infinitival to
+	"WDT":true,  // wh-determiner
+	"WP":true,   // wh-pronoun, personal
+	"WP$":true,  // wh-pronoun, possessive
+	"WRB":true,  // wh-adverb
+	"MD":true,   // verb, modal auxiliary
+}
+
+var basicWords = map[string]bool{
+	"is":  true,
+	"are": true,
+	"be":  true,
+	"has": true,
+	"not":  true,
+	"same": true,
+	"only": true,
+	"use": true,
+	"name": true,
+	"first": true,
+	"one":   true,
+	"zero":  true,
+}
