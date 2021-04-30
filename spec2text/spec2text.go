b0963b33b74b1c76677a7e05ea669a816bdf5449
DQNEO <dqneoo@gmail.com>
2021-04-30 16:09:47 +0900

    Remove first h1 and h2

--- spec2text/spec2text.go
+++ spec2text/spec2text.go
@@ -11,6 +11,8 @@ func GetTextFromHTML(html io.Reader) string {
 		panic(err)
 	}
 	mainTag := gdoc.Find("main")
+	mainTag.Find("h1").Eq(0).Remove() // Title
+	mainTag.Find("h2").Eq(0).Remove() // Date
 	mainTag.Find("pre").Remove()
 	mainTag.Find("code").Remove()
 	text := mainTag.Text()
