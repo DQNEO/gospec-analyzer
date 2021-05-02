04efb1d8ab97d9d3e925cd822db3c6223e083418
DQNEO <dqneoo@gmail.com>
2021-05-02 22:22:33 +0900

    Backport

--- main.js
+++ main.js
@@ -1,45 +1,58 @@
-function textNodesUnder(el) {
-  const a = [];
-  let n;
-  const walk = document.createTreeWalker(el, NodeFilter.SHOW_TEXT, null, false);
-  while ((n = walk.nextNode())) {
-    a.push(n);
+main();
+
+function main() {
+  const nodesToIgnore = new Set(["h2", "h3", "pre"]);
+  const textNodes = collectTextNodes();
+  for (const node of textNodes) {
+    if (nodesToIgnore.has(node.parentNode.localName)) {
+      continue;
+    }
+    processNode(node);
   }
-  return a;
 }
 
-const allTextNodes = textNodesUnder(document.querySelector(".container"));
-
-const skippedNodes = new Set(["h2", "h3", "pre"]);
-
-for (const node of allTextNodes) {
-  const fragment = document.createDocumentFragment();
-  if (skippedNodes.has(node.parentNode.localName)) {
-    continue;
+function collectTextNodes() {
+  const container = document.querySelector(".container")
+  const r = [];
+  let n;
+  const walker = document.createTreeWalker(container, NodeFilter.SHOW_TEXT, null, false);
+  while ((n = walker.nextNode())) {
+    r.push(n);
   }
-  node.textContent.split(/\s+/).forEach((word) => {
+  return r;
+}
+
+function lookupWord(word) {
+    const stem = word2stem[word.toLowerCase()];
+    if (!stem) {
+      return "";
+    }
+    const meaning = dic[stem];
+    if (!meaning) {
+      return "";
+    }
+    return meaning;
+}
+
+function processNode(node) {
+  const fragment = document.createDocumentFragment();
+  const words = node.textContent.split(/\s+/)
+  words.forEach((word) => {
     const span = document.createElement("span");
     span.textContent = word;
     fragment.appendChild(span);
     fragment.appendChild(document.createTextNode(" "));
 
     const trimmedWord = word.replace(/['",.:]/g, '');
-
-    const lword = trimmedWord.toLowerCase();
-    const stem = word2stem[lword];
-    if (!stem) {
-      return;
-    }
-    const meaning = dic[stem];
+    const meaning = lookupWord(trimmedWord);
     if (!meaning) {
       return;
     }
-
     span.className = "word";
-    const label = document.createElement("div");
-    label.textContent = meaning;
-    label.className = "word-label";
-    span.appendChild(label);
+    const tooltip = document.createElement("div");
+    tooltip.textContent = meaning;
+    tooltip.className = "word-translation";
+    span.appendChild(tooltip);
   });
   if (fragment.children.length > 0 && fragment.lastChild.textContent === " ") {
     fragment.removeChild(fragment.lastChild);
